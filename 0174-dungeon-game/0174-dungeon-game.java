63a68149aad1eae8ba14589e744fd8f086d689ce
Ritesh Singh <rihankumar2004@gmail.com>
2026-02-10 20:32:02 +0530

    Time: 2 ms (64.56%), Space: 45.4 MB (54.45%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -11,20 +11,16 @@ class Solution {
         }
         for(int i=m-2;i>=0;i--){
             int val=d[i][n-1];
-            dp[i][n-1]=Math.max(dp[i+1][n-1] - val,1);
-            
+            dp[i][n-1]=Math.max(dp[i+1][n-1] - val,1);  
         }
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                
                 int val=d[i][j];
                 int min=Math.min(dp[i][j+1],dp[i+1][j]);
-                
                 dp[i][j]=Math.max(min-val,1);
                 
             }
         }
         return dp[0][0];
-        
     } 
 }
