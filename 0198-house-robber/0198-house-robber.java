b633436d30c9c66809bfc1f5939d01026ce27640
Ritesh Singh <rihankumar2004@gmail.com>
2026-06-30 23:39:03 +0530

    Time: 0 ms (100%), Space: 42.7 MB (55.8%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,6 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+        if(n == 0)
+        return nums[0];
         if(n == 1)
             return nums[0];
         int[] dp = new int[n];
