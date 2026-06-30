b5a45544bc8c8e24ef2ae6223cf0e4efc132d09c
Ritesh Singh <rihankumar2004@gmail.com>
2026-06-30 21:29:56 +0530

    Time: 10 ms (50.98%), Space: 117.3 MB (38.24%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n, int[] costs) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = Math.max(0, i - 3); j < i; j++) {
+                dp[i] = Math.min(dp[i] , dp[j] + costs[i - 1] + (i - j) * (i - j));
+            }
+        }
+        return dp[n];
+    }
+}
