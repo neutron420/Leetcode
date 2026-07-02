35523993f5a38fe9c4b85c36f66cd561116adf2c
Ritesh Singh <rihankumar2004@gmail.com>
2026-07-02 14:17:59 +0530

    Time: 6 ms (10.88%), Space: 50.2 MB (71.38%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int func(int m, int n, int[][] matrix) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = matrix[i][j];
+                    continue;
+                }
+                int up = Integer.MAX_VALUE;
+                int left = Integer.MAX_VALUE;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = matrix[i][j] + Math.min(up, left);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return func(m, n, grid);
+    }
+}
