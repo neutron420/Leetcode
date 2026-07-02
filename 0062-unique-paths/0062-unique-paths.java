e6b4acccc2f10a0f46924778852eb4b2860b9a69
Ritesh Singh <rihankumar2004@gmail.com>
2026-07-02 12:31:46 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        var f = new int[m][n];
-        f[0][0] = 1;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i > 0) {
-                    f[i][j] += f[i - 1][j];
-                }
-                if (j > 0) {
-                    f[i][j] += f[i][j - 1];
-                }
-            }
+        int N = n + m - 2;
+        int r = m - 1;
+        double res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
         }
-        return f[m - 1][n - 1];
+        return (int)res;
+
     }
 }
