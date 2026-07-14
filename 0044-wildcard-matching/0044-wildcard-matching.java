5a3a067ef117569935c1c22ee6fad66fb1ef7401
Ritesh Singh <rihankumar2004@gmail.com>
2026-07-15 00:21:46 +0530

    Time: 63 ms (11.94%), Space: 48.2 MB (26.52%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,34 +1,36 @@
 class Solution {
-    private Boolean[][] f;
-    private char[] s;
-    private char[] p;
-    private int m;
-    private int n;
-
-    public boolean isMatch(String s, String p) {
-        this.s = s.toCharArray();
-        this.p = p.toCharArray();
-        m = s.length();
-        n = p.length();
-        f = new Boolean[m][n];
-        return dfs(0, 0);
+    public boolean isAllStars(String pattern, int i) {
+        for (int j = 1; j <= i; j++) {
+            if (pattern.charAt(j - 1) != '*')
+                return false;
+        }
+        return true;
     }
+    public boolean isMatch(String s, String p) {
+        int n = p.length();
+        int m = s.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = false;
 
-    private boolean dfs(int i, int j) {
-        if (i >= m) {
-            return j >= n || (p[j] == '*' && dfs(i, j + 1));
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = isAllStars(p, i);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                
+                if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else if (p.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+                } else {
+                    dp[i][j] = false;
+                }
+            }
         }
-        if (j >= n) {
-            return false;
-        }
-        if (f[i][j] != null) {
-            return f[i][j];
-        }
-        if (p[j] == '*') {
-            f[i][j] = dfs(i + 1, j) || dfs(i + 1, j + 1) || dfs(i, j + 1);
-        } else {
-            f[i][j] = (p[j] == '?' || s[i] == p[j]) && dfs(i + 1, j + 1);
-        }
-        return f[i][j];
+        return dp[n][m];
     }
 }
